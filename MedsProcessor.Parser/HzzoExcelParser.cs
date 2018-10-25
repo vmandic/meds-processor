9d5eeb67e0e19927fc9e23a2ad56294b56dc76b4
Vedran Mandić <mandic.vedran@gmail.com>
2018-10-25 18:36:33 +0200

    inital part 3 changes

---
+++ MedsProcessor.Parser/HzzoExcelParser.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MedsProcessor.Common.Models;
+
+namespace MedsProcessor.Parser
+{
+	public class HzzoExcelParser
+	{
+		public Task<ISet<HzzoMedsDownloadDto>> Run(ISet<HzzoMedsDownloadDto> meds)
+		{
+			return null;
+		}
+	}
+}
